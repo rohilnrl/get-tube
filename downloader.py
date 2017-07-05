faadc53a5610044384f7746eee57f25c2e6761bb
Rohil Narula <rohilnrl@gmail.com>
2017-07-05 21:15:25 +0530

    lel

---
+++ downloader.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+
+import hashlib
+import subprocess
+
+
+def gethash(url):
+    try:
+        return hashlib.new(url).hexdigest()
+    except Exception:
+        print('error: failed to generate sha hash')
+
+
+def audio(url, audioquality='0', audioformat='mp3'):
+    outurl = '/'
+
+    getargs = ['youtube-dl', '-x', '--id', '--get-url',
+               '--audio-quality', audioquality,
+               '--audio-format', audioformat]
+
+    getargs.append(url)
+
+    try:
+        outurl = subprocess.check_output(getargs)
+    except subprocess.CalledProcessError:
+        print('error: failed to get audio')
+
+    return outurl
