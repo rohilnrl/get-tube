193ef8a7e17c22bbd395f34645460361fd46d52b
Rohil Narula <rohilnrl@gmail.com>
2017-07-05 12:11:08 +0530

    added output url redirection

--- src/downloader.py
+++ src/downloader.py
@@ -1,17 +1,29 @@
-import os, subprocess, hashlib
+from flask import redirect
+
+import hashlib
+import subprocess
+
 
 def gethash(url):
     try:
         return hashlib.new(url).hexdigest()
     except Exception:
-        print("error: failed to  generate sha hash")
+        print("error: failed to generate sha hash")
+
 
 def audio(url, audioquality="0", audioformat="mp3", *args):
-    getargs = ["youtube-dl", "-x", "-g", "--audio-quality", audioquality, "--audio-format", audioformat]
+    outurl = '/'
+
+    getargs = ["youtube-dl", "-x", "--id", "--get-url",
+               "--audio-quality", audioquality,
+               "--audio-format", audioformat]
+
     getargs.append(args)
     getargs.append(url)
 
-    if(subprocess.call(getargs) == 0):
-        pass
-    else:
-        raise Exception("error: failed to get audio")    
+    try:
+        outurl = subprocess.check_output(getargs)
+    except CalledProcessError:
+        print("error: failed to get audio")
+
+    redirect(outurl)
