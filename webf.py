097cfdfcfab3eb6f029d752f8cca7a1e5b9c8797
Rohil Narula <rohilnrl@gmail.com>
2017-07-05 21:43:00 +0530

    lel

--- webf.py
+++ webf.py
@@ -4,13 +4,15 @@ from downloader import audio
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return open('index.html').read()
-
 @app.route('/audio', methods=['POST'])
 def dl_audio():
     return redirect(audio(request.form["audio"]))
 
+
+@app.route('/')
+def index():
+    return open('index.html').read()
+
+    
 if __name__ == '__main__':
     app.run('127.0.0.1', port=8080, debug=True)
